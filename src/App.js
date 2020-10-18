e94d902e2bf50dc7efbe9f7aa4ca461787ed44e6
Sultan <m.sultan338@gmail.com>
2020-10-18 20:29:02 +0500

    add repo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './Components/NavBar';
+import InfoPanel from './Components/InfoPanel';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <InfoPanel />
+    </div>
+  );
+}
+
+export default App;
